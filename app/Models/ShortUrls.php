2f023c9195ce091fcc5c84dd3034eaaca56b2c84
Mohit Kumar <mohit.handysolver@gmail.com>
2026-04-27 12:03:01 +0530

    Updated composer

--- app/Models/ShortUrls.php
+++ app/Models/ShortUrls.php
@@ -11,18 +11,6 @@ class ShortUrls extends Model
 
     protected $fillable = ['short_code', 'original_url', 'company_id', 'user_id', 'hits'];
 
-    public static function base62($num)
-    {
-        $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $code = '';
-
-        while ($num > 0) {
-            $code = $chars[$num % 62] . $code;
-            $num = intdiv($num, 62);
-        }
-
-        return $code;
-    }
     public function user()
     {
         return $this->belongsTo(User::class);
